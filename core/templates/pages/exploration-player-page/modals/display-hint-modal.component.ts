2c56bba08db3c41c9d2af403791fa2938c0ed614
daniel <daniel.pinto.rodrigues@tecnico.ulisboa.pt>
2025-06-05 14:36:39 +0100

    _audioEndSubject added to AudioPlayerService

--- core/templates/pages/exploration-player-page/modals/display-hint-modal.component.ts
+++ core/templates/pages/exploration-player-page/modals/display-hint-modal.component.ts
@@ -93,6 +93,13 @@ export class DisplayHintModalComponent implements OnDestroy {
         this.isPlaying = false;
       })
     );
+
+    // Subscribe to audio end event.
+    this.directiveSubscriptions.add(
+      this.audioPlayerService.onAudioEnd.subscribe(() => {
+        this.isPlaying = false;
+      })
+    );
   }
 
   ngOnDestroy(): void {
